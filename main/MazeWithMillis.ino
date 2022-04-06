a121066d2c428c398f069c811374316ee3373511
Noizeisdead <noizee829@gmail.com>
2022-04-06 12:59:59 +0200

    Added finish function, maze code now in main

--- main/MazeWithMillis.ino
+++ main/MazeWithMillis.ino
@@ -1,73 +1,12 @@
-//This code makes the robot drive over thin lines. 
-// On the display the values that the LDR picks up will be shown. These values are used to see if the robot follows the line. 
-// This robot uses the left-handed stragety when goeing through a maze. put a delay in de left turn.
 #include <Arduino.h>
-#include <analogWrite.h>
-#include <Adafruit_SSD1306.h>
- 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 32
-#define OLED_RESET     4
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
- 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
- 
-#define LOGO_HEIGHT   16
-#define LOGO_WIDTH    16
- 
-int sensorValueLeft = 39;
-int sensorValueRight = 34;
-int forwardLeft = 16;
-int forwardRight = 18;
-int reverseLeft = 17;
-int reverseRight = 5;
-int greyMin = 60;
-int greyMinR = 50;
-int greyMax = 300;
-int blackMin = 1000;
-unsigned long timer1 = 0;
-unsigned long timer2 = 0;
-unsigned long timer3 = 0;
-unsigned long timer4 = 0;
-unsigned long timer5 = 0;
-unsigned long timer6 = 0;
-unsigned long timer7 = 0;
-unsigned long currentTime;
-String action = "";
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(forwardLeft, OUTPUT);
-  pinMode(forwardRight, OUTPUT);
-  pinMode(reverseLeft, OUTPUT);
-  pinMode(reverseRight, OUTPUT);
-  pinMode(sensorValueLeft, INPUT);
-  pinMode(sensorValueRight, INPUT);
+void setupMaze() {
   action = "";
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Infinite loop to stop the program from executing the rest of the code.
-  }
- 
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
-  display.clearDisplay();
-//Displays left and right LDR values on screen.
- display.setCursor(5, 0);
-drawText("INF1D",4);
-
-  display.display();
-  delay(2000);
-
- 
 }
  
-void loop() {
+void startMaze() {
    display.clearDisplay(); 
-  int sensorValueL = analogRead(sensorValueLeft);
-  int sensorValueR = analogRead(sensorValueRight);
+  int sensorValueL = analogRead(ldrLeft);
+  int sensorValueR = analogRead(ldrRight);
   currentTime = millis();
  
  //Displays left and right LDR values on screen.
@@ -120,6 +59,11 @@ if(action == "turnLeft"){
     action = "turnLeft";
   }else if(sensorValueR > 75 && sensorValueR < 400 && sensorValueL > 70 && sensorValueL < 400){
       action = "uTurn";
+  }else if(sensorValueL < 15 && sensorValueR < 15){
+    stopVehicle();
+    robotStatus = "finished";
+    isDriving = false;
+    isFinished = true;
   }
 
 
